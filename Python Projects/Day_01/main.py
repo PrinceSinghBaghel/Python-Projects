d57c584021a9e6533106c593b2d97c98b3331e3c
PrinceSinghBaghel <princesinghbaghel25@gmail.com>
2025-08-17 11:55:27 +0530

    Day 01

---
+++ Python Projects/Day_01/main.py
@@ -0,0 +1,4 @@
+print("This  is Band  Name Generator  Project")
+city=input("What is the city you grow up  in? \n")
+pet=input ("What is your pet name? \n")
+print(f"Your band name could be  {city} {pet} ")
